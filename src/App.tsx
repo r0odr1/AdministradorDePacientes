f07cb95801492154db6ebab9ec6fb5d36ab341a2
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2025-12-30 10:33:55 -0500

    creando componente para el formulario de pacientes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import PatientForm from "./components/PatientForm"
+import PatientsList from "./components/PatientsList"
 
 
 function App() {
 
   return (
     <>
-      <h1>
-        Hola Mundo!
-      </h1>
+      <div className="container mx-auto mt-20">
+        <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto">
+          Seguimiento de Pacientes {''}
+          <span className="text-emerald-600">Veterinaria</span>
+        </h1>
+
+        <div className="mt-12 md:flex">
+          <PatientForm />
+          <PatientsList />
+
+        </div>
+      </div>
     </>
   )
 }
