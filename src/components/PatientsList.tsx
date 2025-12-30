d1483533d70c21b7d091b4dcc2cd5fc68dd25d49
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2025-12-30 12:43:48 -0500

    escribiendo en el estado de zustand

--- src/components/PatientsList.tsx
+++ src/components/PatientsList.tsx
@@ -1,6 +1,10 @@
-
+import { usePatientStore } from "../store"
 
 export default function PatientsList() {
+  const patients = usePatientStore(state => state.patients)
+
+  console.log(patients);
+
   return (
     <div>PatientsList</div>
   )
