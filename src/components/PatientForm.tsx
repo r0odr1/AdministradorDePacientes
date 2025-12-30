6eec3a99f00aecd28e8b2a1ec9e1bec1c477e520
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2025-12-30 15:56:45 -0500

    Llenar el formulario al editar y actualizarlo

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -2,14 +2,33 @@ import { useForm } from 'react-hook-form'
 import Error from './Error'
 import type { DraftPatient } from '../types'
 import { usePatientStore } from '../store'
+import { useEffect } from 'react'
 
 export default function PatientForm() {
   const addPatient = usePatientStore(state => state.addPatient)
+  const activeId = usePatientStore(state => state.activeId)
+  const patients = usePatientStore(state => state.patients)
+  const updatePatient = usePatientStore(state => state.updatePatient)
 
-  const { register, handleSubmit, formState: {errors}, reset } = useForm<DraftPatient>()
+  const { register, handleSubmit, setValue, formState: {errors}, reset } = useForm<DraftPatient>()
+
+   useEffect(() => {
+    if(activeId) {
+      const activePatient = patients.filter( patient => patient.id === activeId)[0]
+      setValue('name', activePatient.name)
+      setValue('caretaker', activePatient.caretaker)
+      setValue('email', activePatient.email)
+      setValue('date', activePatient.date)
+      setValue('symptoms', activePatient.symptoms)
+    }
+   }, [activeId, patients, setValue])
 
   const registerPatient = (data: DraftPatient) => {
-    addPatient(data)
+    if(activeId) {
+      updatePatient(data)
+    } else {
+      addPatient(data)
+    }
 
     reset()
   }
