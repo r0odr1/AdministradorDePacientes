659fe0993d297fd887839399f4f3f2b7147acb25
Rodrigo Bonilla <rhbon.08@gmail.com>
2025-12-30 22:20:22 -0500

    localStorage al contenido del store de zustand

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand'
-import { devtools } from 'zustand/middleware'
+import { devtools, persist } from 'zustand/middleware'
 import { v4 as uuidv4 } from 'uuid'
 import type { DraftPatient, Patient } from './types'
 
@@ -17,7 +17,8 @@ const createPatient = (patient: DraftPatient) : Patient => {
 }
 
 export const usePatientStore = create<PatientState>()(
-  devtools((set) => ({
+  devtools(
+  persist((set) => ({
     patients: [],
     activeId:'',
 
@@ -52,5 +53,7 @@ export const usePatientStore = create<PatientState>()(
         activeId: ''
       }))
     }
-  }))
-)
+  }), {
+    name: 'patient-storage'
+  })
+))
